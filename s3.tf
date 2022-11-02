98b35a1b287eff31b81fae8093f64a0cf95b4b8f
AlexanderWiechert <awiechert@elastic2ls.com>
2022-11-02 11:07:45 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -2,9 +2,6 @@ resource "aws_s3_bucket" "this" {
   bucket = "${var.project_name}-${var.aws_account_id}"
   
   block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 
   website {
     redirect_all_requests_to = "https://${var.target_domain}"
@@ -30,5 +27,13 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
       kms_master_key_id = aws_kms_key.this.arn
       sse_algorithm     = "aws:kms"
     }
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
