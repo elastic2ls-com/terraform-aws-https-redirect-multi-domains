e050145ec756304327d81a630d1571d671092fe1
AlexanderWiechert <awiechert@elastic2ls.com>
2022-11-02 11:03:38 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -1,5 +1,10 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.project_name}-${var.aws_account_id}"
+  
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 
   website {
     redirect_all_requests_to = "https://${var.target_domain}"
