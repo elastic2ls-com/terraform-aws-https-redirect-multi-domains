89ec3fd0d73edc222c71132d5342021fd03de4a9
AlexanderWiechert <awiechert@elastic2ls.com>
2022-11-04 11:25:08 +0100

    Update s3.tf

--- s3.tf
+++ s3.tf
@@ -38,14 +38,34 @@ resource "aws_s3_bucket_versioning" "this" {
   }
 }
 
+resource "aws_s3_bucket_versioning" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
 resource "aws_s3_bucket" "log_bucket" {
   bucket = "log_bucket"
+  block_public_acls       = true
 }
 
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
   bucket = aws_s3_bucket.log_bucket.id
   acl    = "log-delivery-write"
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "log_bucket" {
+  bucket = aws_s3_bucket.log_bucket.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+
 
 resource "aws_s3_bucket_logging" "this" {
   bucket = aws_s3_bucket.this.id
