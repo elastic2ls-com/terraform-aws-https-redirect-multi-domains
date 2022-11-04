f70f98874891fe5f51b8d9fdafdbf3c3d9b66e8c
AlexanderWiechert <awiechert@elastic2ls.com>
2022-11-04 11:19:17 +0100

    Update cloudfront.tf

--- cloudfront.tf
+++ cloudfront.tf
@@ -13,6 +13,13 @@ resource "aws_cloudfront_distribution" "this" {
   aliases     = [var.target_domain]
   price_class = "PriceClass_100"
   http_version = "http2"
+  
+  logging_config {
+    include_cookies = false
+    bucket          = "logs.s3.amazonaws.com"
+    prefix          = "myprefix"
+  }
+
 
   default_cache_behavior {
     target_origin_id = aws_s3_bucket.this.id
@@ -44,7 +51,15 @@ resource "aws_cloudfront_distribution" "this" {
     cloudfront_default_certificate = false
     acm_certificate_arn      = aws_acm_certificate.cert.arn
     ssl_support_method       = "sni-only"
-    minimum_protocol_version       = "TLSv1.1_2016"
+    minimum_protocol_version       = "TLSv1.2_2019"
 
+  }
+}
+
+resource "aws_s3_bucket" "this" {
+  bucket = "logs"
+
+  tags = {
+    Name = "logs"
   }
 }
