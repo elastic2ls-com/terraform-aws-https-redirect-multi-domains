44a7aa98ac7942df46a44b9e7e033b683c856bf0
AlexanderWiechert <awiechert@elastic2ls.com>
2023-05-12 10:11:08 +0200

    Update s3.tf
    Snyk security fixes

--- s3.tf
+++ s3.tf
@@ -1,10 +1,6 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.project_name}-${var.aws_account_id}"
-  
-  block_public_acls = true
-  aws_s3_bucket_public_access_block = true
-
-  website {
+   website {
     redirect_all_requests_to = "https://${var.target_domain}"
   }
 
@@ -14,6 +10,15 @@ resource "aws_s3_bucket" "this" {
       "Name" = var.project_name
     },
   )
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_kms_key" "this" {
@@ -54,6 +59,15 @@ resource "aws_s3_bucket" "log_bucket" {
   block_public_acls = true
   aws_s3_bucket_public_access_block = true
 
+}
+
+resource "aws_s3_bucket_public_access_block" "log" {
+  bucket = aws_s3_bucket.log_bucket.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_acl" "log_bucket_acl" {
